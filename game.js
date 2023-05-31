d65b98a6acaf2f9f3ccf070efccf43f58f44a1f9
YzCris2023 <crisyyzz2001@gmail.com>
2023-05-31 13:53:54 -0600

    one

---
+++ game.js
@@ -0,0 +1,18 @@
+const canvas= document.querySelector('#game');
+
+const game= canvas.getContext('2d');
+
+
+window.addEventListener('load',startGame)
+function startGame(){
+
+    canvas.se
+    //game.fillRect(0,0,100,100);//lugar donde inicia (x,y)
+    //game.clearRect(0,0,0,0);//borrar de avuerdo a x, y 
+
+    // game.fillStyle='purple';
+    // game.font='25px Verdana';
+    // game.textAlign='left'
+    // game.fillText('Cris',100,100)
+}
+
